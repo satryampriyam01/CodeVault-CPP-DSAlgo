ee6213103b10ca41859604d9d43efad8cccfdb54
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2024-01-18 09:26:34 +0530

    Create All_Are_Same_D1_Code_Forces_GCD_math.cpp

---
+++ All_Are_Same_D1_Code_Forces_GCD_math.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<unordered_map>
+#include<unordered_set>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<string.h>
+#include<vector>
+#include<math.h>
+#include<climits>
+#include<map>
+#include<string.h>
+#include <sstream> //covert string to number //by passing object
+#include<numeric>//lcm;
+#include<set>
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+
+#define MOD 1000000007
+#define ll long long
+#define MM(a) memset(a,0,sizeof(a))
+#define cel(n,k) ((n-1)/k+1)
+#define sets(a) memset(a, -1, sizeof(a))
+#define clr(a) memset(a, 0, sizeof(a))
+#define max(a,b) ((a)>(b)? (a):(b))
+#define min(a,b) ((a)<(b)? (a):(b))
+#define fr(n,i) for(ll i=0;i<n;i++)
+#define fr1(n,i) for(ll i=1;i<=n;i++)
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define mp make_pair
+#define ff first
+#define ss second
+#define f(i,a,b) for(int i = a;i < b;i++)
+#define endl '\n'
+
+using namespace std;
+
+/**
+        Name->Boss Bitch;
+        Id->Satyampriyam01;
+        Rise And Fall Are part of "The Journey";
+**/
+
+const int maxn = 1e5 + 5;
+
+
+
+
+
+
+void solve()
+{
+
+
+    ll n;
+    cin >> n;
+    vector<ll> v(n);
+
+    fr(n, i)
+    {
+        cin >> v[i];
+    }
+
+    sort(all(v));
+
+    vector<ll> b(n - 1);
+
+    for (int i = 1; i < n; i++)
+    {
+        v[i] = v[i] - v[0];
+    }
+
+
+    //required gcd for all elemnt avbove the lowest so subrtract all
+    ll ans = v[1];
+    for (ll i = 1; i < n; i++)
+    {
+        ans = __gcd(ans, v[i]);
+    }
+
+    if (ans != 0)
+    {
+        cout << ans << endl;
+    }
+    else
+    {
+        cout << -1 << endl;
+    }
+
+
+
+}
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll l = 1;
+    cin >> l;
+    while (l--)
+    {
+        solve();
+    }
+    return 0;
+
+}
