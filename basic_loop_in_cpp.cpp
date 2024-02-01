841e5120481bec7bd84d7b1911ad3240da706b74
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2024-02-01 08:51:47 +0530

    Create basic_loop_in_cpp.cpp

---
+++ basic_loop_in_cpp.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b,c,i;
+
+  cout<<"Enter number for loop"<<endl;
+  cin>>a;
+    for(i=0;i<a;i++)
+    {
+      cout<<"for"<<endl;
+          }
+
+
+
+
+cout<<"Enter number for while loop"<<endl;
+cin>>b;
+i=0;
+    while(i<b)
+    {
+      cout<<"while"<<endl;
+      i++;
+      }
+
+cout<<"Enter number for while loop"<<endl;
+      cin>>c;
+      i=0;
+          do
+          {
+            cout<<"while"<<endl;
+            i++;
+          }while(i<c);
+
+
+
+
+
+
+}
